ee62e1f358969639e16d16501f13cdd5c357bc09
asepsetiawan9 <zep.setiawan09@gmail.com>
2026-09-22 09:19:34 +0700

    fix(cors): support production domain sikos.initd.web.id and CORS_ALLOWED_ORIGINS env

--- config/cors.php
+++ config/cors.php
@@ -19,10 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'http://localhost:3000',
-        'http://127.0.0.1:3000',
-    ],
+    'allowed_origins' => array_values(array_filter(array_map('trim', explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:3000,http://127.0.0.1:3000,https://sikos.initd.web.id,http://sikos.initd.web.id'))))),
 
     'allowed_origins_patterns' => [],
 
